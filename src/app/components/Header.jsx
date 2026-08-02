baf053e69ec59bc444e64856b6bffc0ce2ab2455
Victor <victherich@gmail.com>
2026-08-02 03:38:51 +0100

    hp1

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -411,11 +411,11 @@ const router = useRouter();
       {/* Brand Logo Area */}
      <Link href='/'>
       <LogoContainer onClick={closeMenu}>
-        {/* <LogoIcon>
+        <LogoIcon>
           <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 2.69l5.66 5.66a8 8 0 1 1-11.31 0z" />
           </svg>
-        </LogoIcon> */}
+        </LogoIcon>
         <LogoImageWrapper>
           <Image 
           src="/logo.webp"    
